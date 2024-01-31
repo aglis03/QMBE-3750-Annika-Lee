402aa1f51f75ff7e56a21e949fbaea9981bc8083
aglis03 <annikaaglis@gmail.com>
2024-01-31 13:41:36 -0600

    created SQL script

---
+++ SQL Demo.sql
@@ -0,0 +1,16 @@
+# How to make/insert a table in a schema
+
+USE annika_lee;
+
+CREATE TABLE students (age INT, height FLOAT, city VARCHAR (255)); #column name w/ data type
+
+SHOW TABLES;
+
+DROP TABLE students; #drops specific table
+
+DESCRIBE TABLE students;
+
+SELECT *
+FROM annika_lee.students; #to show specific data from schema
+
+INSERT INTO students (age, height, city) VALUES ('20', '5.5', 'Minneapolis');
